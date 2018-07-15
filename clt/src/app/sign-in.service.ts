f36113af1c1fc18a592bfd358828a4a1832a86c6
Tamir Adler <tamiradler@gmail.com>
2018-07-16 00:29:21 +0300

    sign in

---
+++ clt/src/app/sign-in.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+
+declare const gapi: any;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SignInService {
+
+  constructor() {
+    
+  }
+
+  private clientId:string = '322558399464-cjdske3cg04b0o6v2p17k98pp00vn8pl.apps.googleusercontent.com';
+  public auth2: any;
+
+
+
+
+  public attachSignin(element) {
+    this.auth2.attachClickHandler(element, {},
+      (googleUser) => {
+        let profile = googleUser.getBasicProfile();
+        console.log('Token || ' + googleUser.getAuthResponse().id_token);
+        console.log('ID: ' + profile.getId());
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail());
+
+        console.log('googleUser',googleUser);
+        console.log('AuthResponse',googleUser.getAuthResponse());
+        console.log('profile',profile);
+        
+
+      }, (error) => {
+        alert(JSON.stringify(error, undefined, 2));
+      });
+  }
+
+
+
+
+  private scope = [
+    //'profile',
+    //'email',
+    //'https://www.googleapis.com/auth/plus.me',
+    //'https://www.googleapis.com/auth/contacts.readonly',
+    //'https://www.googleapis.com/auth/admin.directory.user.readonly'
+  ].join(' ');
+
+
+  public googleInit(googleButton: any) {
+    gapi.load('auth2', () => {
+      this.auth2 = gapi.auth2.init({
+        client_id: this.clientId,
+        cookiepolicy: 'single_host_origin',
+        scope: this.scope
+      });
+
+      this.attachSignin(googleButton);
+      gapi.signin2.render(googleButton);
+    });
+  }
+}
--- clt/src/app/sign-in/sign-in.component.ts
+++ clt/src/app/sign-in/sign-in.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+import { StringsService } from '../strings.service';
+import { SignInService } from '../sign-in.service';
 
 
-declare const gapi: any;
 
 @Component({
   selector: 'app-sign-in',
@@ -10,63 +11,33 @@ declare const gapi: any;
 })
 export class SignInComponent implements OnInit, AfterViewInit {
 
-  constructor(private element: ElementRef) {
-    console.log('ElementRef: ', this.element);
+  constructor(private stringService: StringsService, private signInService: SignInService) {
+    
   }
+
 
   ngOnInit() {
   }
 
+
+
+
+
   ngAfterViewInit(): void {
-    this.googleInit();
-  }
-
-  private clientId:string = '322558399464-cjdske3cg04b0o6v2p17k98pp00vn8pl.apps.googleusercontent.com';
-
-  private scope = [
-    //'profile',
-    //'email',
-    //'https://www.googleapis.com/auth/plus.me',
-    //'https://www.googleapis.com/auth/contacts.readonly',
-    //'https://www.googleapis.com/auth/admin.directory.user.readonly'
-  ].join(' ');
-
-  public auth2: any;
-
-  public googleInit() {        
-    gapi.load('auth2', () => {
-      this.auth2 = gapi.auth2.init({
-        client_id: this.clientId,
-        cookiepolicy: 'single_host_origin',
-        scope: this.scope
-      });
-      var googleButton = document.getElementById("google-button");
-      this.attachSignin(googleButton);
-      gapi.signin2.render(googleButton);
-    });
+    var googleButton = document.getElementById("google-button");
+    this.signInService.googleInit(googleButton);
   }
 
 
-  public attachSignin(element) {
-    this.auth2.attachClickHandler(element, {},
-      (googleUser) => {
 
-        let profile = googleUser.getBasicProfile();
-        console.log('Token || ' + googleUser.getAuthResponse().id_token);
-        console.log('ID: ' + profile.getId());
-        console.log('Name: ' + profile.getName());
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
 
-        console.log('googleUser',googleUser);
-        console.log('AuthResponse',googleUser.getAuthResponse());
-        console.log('profile',profile);
-        
 
-      }, (error) => {
-        alert(JSON.stringify(error, undefined, 2));
-      });
-  }
+
+
+
+
+
+
 
 
 
