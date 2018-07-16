ed5d6c104651d9b9fd9297f6f94a124299a93ab2
Tamir Adler <tamiradler@gmail.com>
2018-07-16 05:01:33 +0300

    refactoring

---
+++ srv/src/main/java/com/minyan/karov/services/synagogue/AddSynagogueOutput.java
@@ -0,0 +1,30 @@
+package com.minyan.karov.services.synagogue;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.minyan.karov.services.PutError;
+
+public class AddSynagogueOutput implements PutError {
+	private Map <String, List<String>> errors = new HashMap<>();
+
+	@Override
+	public void putError(String elmId, String error) {
+		List<String> errorsList =  errors.get(elmId);
+		if (errorsList == null) {
+			errorsList = new ArrayList<>();
+			errors.put(elmId, errorsList);
+		}
+		errorsList.add(error);
+	}
+	
+	public Map<String, List<String>> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(Map<String, List<String>> errors) {
+		this.errors = errors;
+	}
+}
