4361220746aeac71588b09042796cf8effe3b66c
Tamir Adler <tamiradler@gmail.com>
2018-07-23 06:01:31 +0300

    v

--- clt/src/app/synagogue-page/synagogue-page.component.ts
+++ clt/src/app/synagogue-page/synagogue-page.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { StringsService } from '../strings.service';
+import { GetAllSynagoguesOutput } from '../GetAllSynagoguesOutput';
+import { environment } from '../../environments/environment';
+import { Synagogue } from '../Synagogue';
 
 @Component({
   selector: 'app-synagogue-page',
@@ -9,12 +13,27 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SynagoguePageComponent implements OnInit {
 
-  constructor(private activatedRoute: ActivatedRoute,
-    private http: HttpClient
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient,
+    public stringService: StringsService
   ) { }
 
+  synagogueId: string;
+  synagogue: Synagogue;
+
   ngOnInit() {
-    var synagogueId:string = this.activatedRoute.snapshot.paramMap.get('synagogueId');
+    this.synagogueId = this.activatedRoute.snapshot.paramMap.get('synagogueId');
+    this.getSynagogue();
+  }
+
+  getSynagogue() {
+    return this.http.get<GetAllSynagoguesOutput>(environment.hostUrl+'getSynagogue/'+this.synagogueId).subscribe(res => 
+      {
+        res.synagogues.forEach(synagogue => {
+          this.synagogue = synagogue;
+        });
+      });;
   }
 
 }
