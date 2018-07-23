ee8f9f1028d0890268f54a04412e392050731dc7
Tamir Adler <tamiradler@gmail.com>
2018-07-23 04:15:35 +0300

    v

--- clt/src/app/app-routing.module.ts
+++ clt/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { SenagogMapComponent } from './senagog-map/senagog-map.component';
 import { AddSynagogueComponent } from './add-synagogue/add-synagogue.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { SynagoguePageComponent } from './synagogue-page/synagogue-page.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/SenagogMap', pathMatch: 'full' },
   { path: 'SenagogMap', component: SenagogMapComponent },
   { path: 'addSenagog', component: AddSynagogueComponent },
+  { path: 'synagoguePage', component: SynagoguePageComponent },
   { path: 'signIn', component: SignInComponent }
 ];
 
