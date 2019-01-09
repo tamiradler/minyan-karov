be1016b7f4ab6464bb27de8a8d297e68fb02f541
Tamir Adler <tamiradler@gmail.com>
2019-01-09 23:08:40 +0200

    f

--- srv/src/main/java/com/minyan/karov/ServletInitializer.java
+++ srv/src/main/java/com/minyan/karov/ServletInitializer.java
@@ -3,12 +3,12 @@ package com.minyan.karov;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-public class ServletInitializer extends SpringBootServletInitializer 
+public class ServletInitializer //extends SpringBootServletInitializer 
 {
-	@Override
+	/*@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) 
 	{
 		return application.sources(Application.class);
-	}
+	}*/
 
 }
